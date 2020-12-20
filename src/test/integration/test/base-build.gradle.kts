7e66552f82c15de04b2e06b6e3b3c673d6efd653
soloturn <soloturn@gmail.com>
2020-12-20 11:23:20 +0100

    testcases now use newest gradle

---
+++ src/test/integration/test/base-build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+  id("fr.brouillard.oss.gradle.jgitver") version "0.9.1"
+}
+
+repositories {
+  mavenLocal()
+  maven {
+    url = uri("https://plugins.gradle.org/m2/")
+  }
+  jcenter()
+}
